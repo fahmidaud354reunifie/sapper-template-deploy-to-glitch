eaa738d1767a2cc5deb38b668c5e38c114e65073
fahmidaud354reunifie <73093279+fahmidaud354reunifie@users.noreply.github.com>
2021-03-02 10:25:33 +0700

    First commit

---
+++ src/components/Card.svelte
@@ -0,0 +1,18 @@
+<script type="text/javascript">
+	// export let title, caption
+	export let object
+	console.log(object)
+</script>
+
+
+<!-- <h1>{title}</h1>
+
+<p>{caption}</p> -->
+
+{#if object}
+{#each object as o}
+<h1>{o.title}</h1>
+
+<p>{o.caption}</p>
+{/each}
+{/if}
---
+++ src/routes/about.svelte
@@ -0,0 +1,20 @@
+<script type="text/javascript">
+	import Card from '../components/Card.svelte'
+
+	// const title = "About this site"
+	// const caption = "This is the 'about' page. There's not much here."
+
+	const object = [{
+		title: "About this site",
+		caption: "This is the 'about' page. There's not much here.",
+	}]
+</script>
+
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<!-- <Card title={title} caption={caption} /> -->
+<!-- {#if object} -->
+<Card object={object} />
+<!-- {/if} -->
